c32ed1fea02b1eec5365c9a5394585d48e27bae8
elias1991xNataly <eliasalfonso1991@gmail.com>
2023-07-19 13:20:16 +0200

    adding functions

--- src/components/Contador/Contador.js
+++ src/components/Contador/Contador.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import React from 'react';
 
-const Contador = ()=>{
-const [contador,setContador]=useState(0);
+const Contador = () => {
+	const [contador, setContador] = useState(0);
+	//función para sumar uno
+	function addOne() {
+		setContador(prevCount => prevCount + 1)
+	}
+	//función para restar uno
+	function restOne() {
+		setContador(prevCount => contador !== 0 ? prevCount - 1 : prevCount)
+	}
 
 
 
-	return(<div>
-<button onClick={()=>{setContador(prevCount=>prevCount+1)}}>Incrementar</button>
+	return (<div>
+		<button onClick={() => addOne()}>Incrementar</button>
 
-{contador}
-<button onClick={()=>{setContador(prevCount=>contador!==0?prevCount-1:prevCount)}}>Disminuir</button>
+		{contador}
+		<button onClick={() => restOne()}>Disminuir</button>
 
 	</div>)
 }
